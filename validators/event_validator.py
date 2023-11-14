482bab5e643b9e3ee37e6f18dedf88ec00f04b50
EdiBOI25 <jaguarul2501@gmail.com>
2023-11-14 21:25:16 +0200

    created validator class

--- validators/event_validator.py
+++ validators/event_validator.py
@@ -1,20 +1,22 @@
 from domain.event import Event
 
 
-def validate_event(event: Event):
-    """
-    Valideaza datele unui eveniment
-    :param event: evenimentul
-    Exceptions: daca datele sunt goale
-    """
-    errors = ''
-    if event.get_id() == '':
-        errors += 'ID-ul nu poate fi gol'
-    if event.get_date() == '':
-        errors += 'Data nu poate fi goala'
-    if event.get_duration() == '':
-        errors += 'Durata nu poate fi goala'
-    if event.get_description() == '':
-        errors += 'Descrierea nu poate fi goala'
-    if len(errors) > 0:
-        raise ValueError(errors)
+class EventValidator:
+    @staticmethod
+    def validate_event(event: Event):
+        """
+        Valideaza datele unui eveniment
+        :param event: evenimentul
+        Exceptions: daca datele sunt goale
+        """
+        errors = ''
+        if event.get_id() == '':
+            errors += 'ID-ul nu poate fi gol'
+        if event.get_date() == '':
+            errors += 'Data nu poate fi goala'
+        if event.get_duration() == '':
+            errors += 'Durata nu poate fi goala'
+        if event.get_description() == '':
+            errors += 'Descrierea nu poate fi goala'
+        if errors:
+            raise ValueError(', '.join(errors))
--- validators/person_validator.py
+++ validators/person_validator.py
@@ -1,18 +1,20 @@
 from domain.person import Person
 
 
-def validate_person(person: Person):
-    """
-    Valideaza datele unei persoane
-    :param person: persoana
-    Exceptions: daca datele sunt goale
-    """
-    errors = ''
-    if person.get_id() == '':
-        errors += 'ID-ul nu poate fi gol'
-    if person.get_name() == '':
-        errors += 'Numele nu poate fi gol'
-    if person.get_address() == '':
-        errors += 'Adresa nu poate fi goala'
-    if len(errors) > 0:
-        raise ValueError(errors)
+class PersonValidator:
+    @staticmethod
+    def validate_person(person: Person):
+        """
+        Valideaza datele unei persoane
+        :param person: persoana
+        Exceptions: daca datele sunt goale
+        """
+        errors = ''
+        if person.get_id() == '':
+            errors += 'ID-ul nu poate fi gol'
+        if person.get_name() == '':
+            errors += 'Numele nu poate fi gol'
+        if person.get_address() == '':
+            errors += 'Adresa nu poate fi goala'
+        if errors:
+            raise ValueError(', '.join(errors))
