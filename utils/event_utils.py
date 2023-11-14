5ed5817a45b2bd4c9b256c0e08a2a9e1175f03da
EdiBOI25 <jaguarul2501@gmail.com>
2023-11-14 13:24:03 +0200

    added check for __eq__ if its instance

--- utils/event_utils.py
+++ utils/event_utils.py
@@ -1,26 +1,6 @@
 from domain.event import Event
 
 
-def add_event(event_list, event: Event):
-    if event in event_list:
-        print(f'Evenimentul se afla deja in lista.')
-        return
-    event_list.append(event)
-
-
-def remove_event(event_list, event: Event):
-    if event not in event_list:
-        print(f'Evenimentul nu a fost gasit in lista.')
-        return
-    event_list.remove(event)
-
-
-def search_event_by_id(event_list, idcode) -> Event:
-    for event in event_list:
-        if event.get_id() == idcode:
-            return event
-
-
 def generate_id(event_list):
     """
     Genereaza un ID nou
--- utils/person_utils.py
+++ utils/person_utils.py
@@ -2,23 +2,6 @@ from domain.person import Person
 from validators.person_validator import *
 
 
-def update_person(person: Person) -> Person:
-    """
-    Modifica datele unei persoane
-    :param person:
-    :return:
-    """
-    try:
-        validate_person(person)
-        return person
-    except ValueError:
-        pass
-
-
-def update_person_name(person: Person, name):
-    pass
-
-
 def generate_id(person_list):
     """
     Genereaza un ID nou
