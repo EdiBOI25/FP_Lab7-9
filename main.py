3828fd7ec15cfd7674c1815f40021096eba3e8d6
EdiBOI25 <jaguarul2501@gmail.com>
2023-11-18 14:10:13 +0200

    updated todo list

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from tests.test_main import execute_all_tests
 from ui.ui_class import UI
-# TODO 3: fa un "register" repo care sa contina o lista de perechi de genu (id_client, id_event)
-#   fa testele pt registration repository
 # TODO 4: refa testele astfel incat la erori sa faci assert err == 'Asta e eroarea', adauga comentarii la functii
 # TODO 5: fa cerintele alealalte (rapoarte etc)
 
